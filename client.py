886fe23fb034c772a157c7d51e56167542b55726
reza <reza.pp.9522@gmail.com>
2024-07-22 00:53:17 +0330

    added last data's to the repository

--- client.py
+++ client.py
@@ -1,25 +1,11 @@
 import socket
 import time
 import pandas as pd
+import os
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(('localhost', 9080))
-
-# تا زمانی که خودم نگفتم پیام بفرست
-# while True:
-#    #send message to server
-#    message = input("Enter a message:")
-#    client.send(message.encode("UTF-8"))
-#    if message == "quit":
-#       break
-   
-#    #recive message from server
-#    response = client.recv(1024).decode("UTF-8")
-#    print("responce",response)
-
-# client.close()
-
 
 #send message to server
 message = "Start program"
@@ -34,10 +20,24 @@ time.sleep(100)
 response = client.recv(1024).decode("UTF-8")
 print("responce",response)
 df_final_data = pd.read_csv(response, encoding='utf-8')
-print(df_final_data)
+
+
+image_data = b""
+while True:
+    data = client.recv(1024)
+    if not data:
+        break
+    image_data += data
+
+# ذخیره داده‌های باینری به عنوان فایل تصویری
+with open("received_output1.png", "wb") as file:
+    file.write(image_data)
+
 
 message = "received_successfully"
 client.send(message.encode("UTF-8"))
 
 
 client.close()
+
+os.startfile("received_output1.png")
